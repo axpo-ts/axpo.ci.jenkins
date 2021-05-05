9120f22ffff690ce5b93b4305ce0883658bb4eb9
Peter Samuel TCI-MQ <Samuel.Peter@axpo.group>
2021-05-05 14:14:03 +0200

    testing around env vras

--- vars/setProjectName.groovy
+++ vars/setProjectName.groovy
@@ -1,7 +1,8 @@
 def call() {
   script {
     allJob = env.JOB_NAME.tokenize('/') as String[];
-    env.PROJECT_NAME = allJob[0];
-    echo "Project name: ${env.PROJECT_NAME}"
+    //env.PROJECT_NAME = allJob[0];
+    name = allJob[0];
+    echo "Project name: ${name}"
   }
 }
