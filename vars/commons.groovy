9db840c36b90470cc58b87fc5bbd7d2a015db6a1
Samuel Peter <Samuel.Peter@axpo.com>
2022-10-17 08:42:52 +0000

    update credentials ID for bitbucket

--- vars/commons.groovy
+++ vars/commons.groovy
@@ -105,7 +105,7 @@ def pushTag() {
   echo "create new git tag"
   powershell "git tag -f ${env.GIT_VERSION}"
   gitUrlBase = "${GIT_URL}".split("//")[1]
-  withCredentials([usernamePassword(credentialsId: 'c2457393-c808-4b22-a3a6-26316ad4e562', usernameVariable: 'GIT_USR', passwordVariable: 'GIT_PWD')]) {
+  withCredentials([usernamePassword(credentialsId: 'bb-system_jenkins_bitbucket', usernameVariable: 'GIT_USR', passwordVariable: 'GIT_PWD')]) {
     powershell("git push https://${GIT_USR}:${GIT_PWD}@${gitUrlBase} :refs/tags/${env.GIT_VERSION}")
     powershell("git push https://${GIT_USR}:${GIT_PWD}@${gitUrlBase} ${env.GIT_VERSION}")
   }
