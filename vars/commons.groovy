e234b0ef209faab646b9a9b39ee7cc8bbab5bab9
Salah Khalfan <khalfan.salah@axpo.com>
2024-12-04 11:22:24 +0100

    Enable github push support

--- vars/commons.groovy
+++ vars/commons.groovy
@@ -112,14 +112,39 @@ def octoUpload(Map args) {
   }
 }
 
+// def pushTag() {
+//   echo "create new git tag"
+//   powershell "git tag -f ${env.GIT_VERSION}"
+//   gitUrlBase = "${GIT_URL}".split("//")[1]
+//   withCredentials([usernamePassword(credentialsId: 'bb-system_jenkins_bitbucket', usernameVariable: 'GIT_USR', passwordVariable: 'GIT_PWD')]) {
+//     powershell("git push https://${GIT_USR}:${GIT_PWD}@${gitUrlBase} :refs/tags/${env.GIT_VERSION}")
+//     powershell("git push https://${GIT_USR}:${GIT_PWD}@${gitUrlBase} ${env.GIT_VERSION}")
+//   }
+// }
+
 def pushTag() {
-  echo "create new git tag"
-  powershell "git tag -f ${env.GIT_VERSION}"
-  gitUrlBase = "${GIT_URL}".split("//")[1]
-  withCredentials([usernamePassword(credentialsId: 'bb-system_jenkins_bitbucket', usernameVariable: 'GIT_USR', passwordVariable: 'GIT_PWD')]) {
-    powershell("git push https://${GIT_USR}:${GIT_PWD}@${gitUrlBase} :refs/tags/${env.GIT_VERSION}")
-    powershell("git push https://${GIT_USR}:${GIT_PWD}@${gitUrlBase} ${env.GIT_VERSION}")
-  }
+    echo "Create new git tag"
+    powershell "git tag -f ${env.GIT_VERSION}"
+
+    // Extract base URL
+    gitUrlBase = "${GIT_URL}".split("//")[1]
+
+    if (gitUrlBase.contains("bitbucket.org")) {
+        echo "Using Bitbucket for authentication"
+        withCredentials([usernamePassword(credentialsId: 'bb-system_jenkins_bitbucket', usernameVariable: 'GIT_USR', passwordVariable: 'GIT_PWD')]) {
+            powershell("git push https://${GIT_USR}:${GIT_PWD}@${gitUrlBase} :refs/tags/${env.GIT_VERSION}")
+            powershell("git push https://${GIT_USR}:${GIT_PWD}@${gitUrlBase} ${env.GIT_VERSION}")
+        }
+    } else if (gitUrlBase.contains("github.com")) {
+        echo "Using GitHub for authentication"
+        withCredentials([string(credentialsId: 'github_personal_access_token', variable: 'GIT_TOKEN')]) {
+            powershell("git push https://${GIT_TOKEN}@${gitUrlBase} :refs/tags/${env.GIT_VERSION}")
+            powershell("git push https://${GIT_TOKEN}@${gitUrlBase} ${env.GIT_VERSION}")
+        }
+    } else {
+        error "Unsupported git provider in URL: ${gitUrlBase}"
+    }
 }
+
 
 return this
